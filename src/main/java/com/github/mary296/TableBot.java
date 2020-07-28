10e1c20a9165eaaeddefabdc5eec0b6d84a349d9
Mary <guskowa.masha@yandex.ru>
2020-07-28 12:09:08 +0700

    Parse time table

--- src/main/java/com/github/mary296/TableBot.java
+++ src/main/java/com/github/mary296/TableBot.java
@@ -5,17 +5,33 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.util.Map;
+
 public class TableBot extends TelegramLongPollingBot {
     public String getBotToken() {
         return "1340975056:AAHM2iuQdbWI-i3L7fl7lgWdQd4fIU7AGIs";
     }
 
     public void onUpdateReceived(Update update) {
-        SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
+        Map<String, String> timeTable = TimeTableParser.getTodayTimeTable();
+        StringBuilder messageBuilder = new StringBuilder();
+        timeTable.forEach((time, lesson) -> {
+            if (!lesson.isEmpty()) {
+                messageBuilder.append("`");
+                messageBuilder.append(String.format("%5s", time));
+                messageBuilder.append("  ");
+                messageBuilder.append(lesson);
+                messageBuilder.append("`");
+                messageBuilder.append("\n");
+            }
+        });
+        String message = messageBuilder.toString();
+        SendMessage command = new SendMessage() // Create a SendMessage object with mandatory fields
                 .setChatId(update.getMessage().getChatId())
-                .setText(update.getMessage().getText());
+                .setParseMode("Markdown")
+                .setText(message);
         try {
-            execute(message); // Call method to send the message
+            execute(command); // Call method to send the message
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
---
+++ src/main/java/com/github/mary296/TimeTableParser.java
@@ -0,0 +1,68 @@
+package com.github.mary296;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.time.LocalDate;
+import java.time.format.TextStyle;
+import java.util.LinkedHashMap;
+import java.util.Locale;
+import java.util.Map;
+
+
+public class TimeTableParser {
+    public static Map<String, String> getTodayTimeTable() {
+        String siteUrl = "https://table.nsu.ru/group/17112";
+
+        Document doc;
+        try {
+            doc = Jsoup.connect(siteUrl).get();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        Element table = doc.selectFirst("table.time-table");
+
+        String today = getToday();
+
+        int dayOfWeekIndex = getTodayTimeTableColumn(table, today);
+
+        Map<String, String> timeTable = new LinkedHashMap<>();
+
+        table.child(1).children().stream().skip(1).forEach(element -> {
+            String time = element.child(0).text();
+
+            String lesson = element.child(dayOfWeekIndex).text();
+
+            timeTable.put(time, lesson);
+        });
+
+        return timeTable;
+    }
+
+    private static String getToday() {
+        return LocalDate.now()
+                .getDayOfWeek()
+                .getDisplayName(TextStyle.FULL, new Locale("ru", "RU"));
+    }
+
+    private static int getTodayTimeTableColumn(Element timeTable, String today) {
+        int dayOfWeekIndex = 0;
+
+        Elements daysRow = timeTable.child(1).child(0).children();
+
+        for (Element element : daysRow) {
+            System.out.println(element);
+
+            if (element.text().equalsIgnoreCase(today)) {
+                return dayOfWeekIndex;
+            }
+
+            dayOfWeekIndex++;
+        }
+
+        throw new RuntimeException("Today column in time table not found.");
+    }
+}
